a39a28bfd100d67c7c87adaea61226c71f08e8bd
pathapati-mahesh <dataanalystentry@gmail.com>
2024-11-01 09:37:13 +0100

    Created a New Reference App

---
+++ C_Sharp/OperatorsApp/ChildOperatorsFile/Methods.cs
@@ -0,0 +1,40 @@
+﻿namespace ChildOperatorsFile
+{
+    public class Methods
+    {
+        public int AddFunc(int a, int b)
+        {
+            return a + b;
+        }
+        public int SubstractFunc(int a, int b)
+        {
+            return a - b;
+        }
+        public int MultiplyFunc(int a, int b)
+        {
+            return a * b;
+        }
+        public int DivideFunc(int a, int b)
+        {
+            return a / b;
+        }
+        public int RemaindeFunc(int a, int b)
+        {
+            return a % b;
+        }
+        public int IncrementFunc(int a, int b)
+        {
+            int PostIncrementOpp = (a++) + (b++);
+            int PreIncrementOpp = (++a) + (++b);
+            int SumOfBoth =PreIncrementOpp + PostIncrementOpp;
+            return SumOfBoth;
+
+        }
+        public int DecrementFunc(int a, int b) {
+            int PostDecrementOpp = (a--) + (b--);
+            int PreDecrementOpp = (--a) + (--b);
+            int SumOfBoth = PreDecrementOpp + PostDecrementOpp;
+            return SumOfBoth;
+        }
+    }
+}
---
+++ C_Sharp/OperatorsApp/OperatorsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using ChildOperatorsFile;
+
+
+namespace OperatorsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            Console.WriteLine($"Addition of Two Numbers :{ methods.AddFunc(2,3)}");
+            Console.WriteLine($"Substraction of Two Numbers :{methods.SubstractFunc(2, 3)}");
+            Console.WriteLine($"Multiplication of Two Numbers :{methods.MultiplyFunc(2, 3)}");
+            Console.WriteLine($"Division of Two Numbers :{methods.DivideFunc(3, 2)}");
+            Console.WriteLine($"Remainder of Two Numbers :{methods.RemaindeFunc(3, 2)}");
+            Console.WriteLine($"Increment of Two Numbers (Preincrement & Post Increment) :{methods.IncrementFunc(2, 1)}");
+            Console.WriteLine($"Increment of Two Numbers (Pre Decrement & Post Decrement) :{methods.DecrementFunc(2, 1)}");
+        }
+    }
+}
