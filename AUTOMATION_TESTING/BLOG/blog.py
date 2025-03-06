05f01c9208155f15422be3678070f77d7f12c7cc
pathapati-mahesh <maheshkumarpathapati2@gmail.com>
2025-03-06 20:59:46 +0100

    Added testmethod to blog

--- AUTOMATION_TESTING/BLOG/blog.py
+++ AUTOMATION_TESTING/BLOG/blog.py
@@ -1,2 +1,21 @@
 from unittest import TestCase
 class Blog(TestCase):
+    def __init__(self, name,profession,hobbies,sequence):
+        self.name=name
+        self.profession=profession
+        self.hobbies=hobbies
+        self.evensequence=[]
+        self.oddsequence=[]
+        self.sequence=sequence
+    
+    def form_data(self):
+        data=f"Hello My Name is {self.name} and I'm a {self.profession}. My Hobbies are{self.hobbies}"
+        return data
+
+    def define_sequence(self):
+        for num in self.sequence:
+            if num%2==0:
+                self.evensequence.append(num)
+            else:
+                self.oddsequence.append(num)
+        return (self.evensequence,self.oddsequence)
