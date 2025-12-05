8c2c639c0ac9aa235a83ff93c987f1b557ee2656
pathapati-mahesh <maheshkumarpathapati2@gmail.com>
2025-12-05 11:00:00 +0100

    Testing

--- C_Sharp/MethodsApp/Program.cs
+++ C_Sharp/MethodsApp/Program.cs
@@ -8,3 +8,11 @@ for (int j = 5; j > 0; j--)
 {
     Console.WriteLine($"Countdown {j}");
 }
+for (int k = 0; k <= 10; k += 2)
+{
+    Console.WriteLine($"Even Number: {k}");
+}
+for (int m = 10; m >= 0; m -= 2)
+{
+    Console.WriteLine($"Even Countdown: {m}");
+}
